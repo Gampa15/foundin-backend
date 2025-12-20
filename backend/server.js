00445d419ff9e92660a5a90beb0cb45555a777c9
Akhila <gampaakhila1@gmail.com>
2025-12-20 20:58:29 +0530

    Fix server start and dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,7 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const app = require('./src/app');
 
-dotenv.config();
+const PORT = process.env.PORT || 10000;
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-const PORT = process.env.PORT || 5000;
-const http = require('http');
-const socket = require('./src/socket');
-
-const server = http.createServer(app);
-socket.init(server);
-
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
