60b13964efeec9493a232b69917bf8fb7b07567c
Akhila <gampaakhila1@gmail.com>
2025-12-28 18:48:10 +0530

    Fix MongoDB connection on startup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-const app = require('./src/app');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 10000;
+const app = require('./src/app');
+const connectDB = require('./src/config/db');
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
