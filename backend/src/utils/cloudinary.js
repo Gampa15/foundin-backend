e610ed07636ce5bc0403245f40b8b564a7b92816
Akhila <gampaakhila1@gmail.com>
2026-02-05 18:52:21 +0530

    Add cloudinary utils

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+const cloudinary = require('cloudinary').v2;
+
+const hasUrl = Boolean(process.env.CLOUDINARY_URL);
+const hasCreds = Boolean(
+  process.env.CLOUDINARY_CLOUD_NAME &&
+    process.env.CLOUDINARY_API_KEY &&
+    process.env.CLOUDINARY_API_SECRET
+);
+
+const cloudinaryEnabled = hasUrl || hasCreds;
+
+if (hasCreds) {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
+}
+
+const uploadBuffer = (buffer, options = {}) =>
+  new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(options, (err, result) => {
+      if (err) return reject(err);
+      return resolve(result);
+    });
+    stream.end(buffer);
+  });
+
+module.exports = {
+  cloudinary,
+  cloudinaryEnabled,
+  uploadBuffer
+};
