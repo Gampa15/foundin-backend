abf68911fcea13419928b0c07c5daba34b80a7d0
Akhila <gampaakhila1@gmail.com>
2026-02-05 19:50:51 +0530

    Add cloudinary utils

--- backend/src/utils/upload.js
+++ backend/src/utils/upload.js
@@ -1,14 +1,6 @@
 const multer = require('multer');
-const uploadDir = require('./uploadsDir');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
